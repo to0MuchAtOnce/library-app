737829fad93fb60bf35d10543e7cd5a543c4afe3
Tom lewis <tom.lewis488@gmail.com>
2024-07-04 22:16:20 +0200

    Re-factor project

--- index.js
+++ index.js
@@ -1,63 +1,86 @@
 const cards = document.querySelector('.cards');
 const table = document.getElementById('table');
-const btn = document.querySelector('.btn');
+const addBtn = document.querySelector('.addBtn');
+const cancelBtn = document.querySelector('.cancelBtn');
+const addNewBook = document.querySelector('.add-new-book');
+const modal = document.querySelector('.modal');
 
 const titleField = document.getElementById('titleField');
 const authorField = document.getElementById('authorField');
 const pagesField = document.getElementById('pagesField');
-const isRead = document.getElementById('yes');
+const form = document.getElementById('form');
 
 titleField.value = '';
 authorField.value = '';
 pagesField.value = '';
-isRead.value = '';
 
 const myLibrary = [];
 
-function Book(title, author, numPages, isRead) {
+function Book(title, author, numPages, readStatus) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.isRead = isRead;
+  this.readStatus = readStatus;
   this.info = function () {
     return `${this.title}, ${this.author}, ${this.numPages}, ${this.isRead}`;
   };
-
-  console.log('info', this.info);
 }
 
-function addBookToLibrary(title, author, numPages, isRead) {
-  let row = table.insertRow();
-  let titleCell = row.insertCell(0);
-  let authorCell = row.insertCell(1);
-  let numPagesCell = row.insertCell(2);
-  let isReadCell = row.insertCell(3);
-
-  titleCell.innerHTML = title;
-  authorCell.innerHTML = author;
-  numPagesCell.innerHTML = numPages;
-  isReadCell.innerHTML = isRead ? 'Yes' : 'No';
-
-  const book = new Book(title, author, numPages, isRead);
-  myLibrary.push(book);
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  renderLibrary();
 }
 
-btn.addEventListener('click', (e) => {
+function renderLibrary() {
+  const table = document.querySelector('#tableBody');
+  table.innerHTML = '';
+  myLibrary.forEach((book, index) => {
+    const bookRow = document.createElement('tr');
+    bookRow.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.numPages}</td>
+    <td>${book.readStatus}</td>
+    <td><button
+      class="delete"
+      id="delete"
+      onclick="deleteBook(${index})"
+    >Delete Book</button></td>`;
+    table.appendChild(bookRow);
+  });
+}
+
+addBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  addBookToLibrary(
-    titleField.value,
-    authorField.value,
-    pagesField.value,
-    isRead.checked
-  );
-  console.log('is Read: ', isRead.checked);
+  const title = titleField.value;
+  const author = authorField.value;
+  const numPages = pagesField.value;
+  const readStatus = document.getElementById('yes').checked ? 'Yes' : 'No';
+
+  const newBook = new Book(title, author, numPages, readStatus);
+
+  addBookToLibrary(newBook);
 });
 
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Not read');
-// myLibrary.push(theHobbit);
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  renderLibrary();
+}
 
-// const trainspotting = new Book('Trainspotting', 'Irvine Welsh', '344', 'Read');
-// myLibrary.push(trainspotting);
+addNewBook.addEventListener('click', () => {
+  modal.style.display = 'block';
+  if ((modal.style.display = 'block')) {
+    addNewBook.style.display = 'none';
+  }
+});
 
-// const terrorist = new Book('Terrorist', 'John Updike', '320', 'Not read');
-// myLibrary.push(terrorist);
+cancelBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+  if ((modal.style.display = 'none')) {
+    if ((addNewBook.style.display = 'none')) {
+      addNewBook.style.display = 'block';
+    } else {
+      cancelNewBook.style.display = 'none';
+    }
+  }
+});
